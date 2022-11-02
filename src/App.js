3b36ffd7ccade67f5e5ac66993ca4627e8b23b8f
Zharyk <aijoka99@gmail.com>
2022-11-02 20:56:50 +0600

    task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import data from './mock_stores.json'
 
 function App() {
+  const [mock, setMock] = useState(data)
+
+  useEffect(() => { mock.reduce((items, acc) => acc + items.value) }, [])
+  console.log(mock);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {mock.map(items => (<div key={items.store.id}>
+        <p>{items.store.name}</p>
+        {items.months.map(item => <p>{item.name}<input value={item.value} onChange={(e) => setMock(e.target.value)} /></p>)}
+
+
+      </div>))}
     </div>
   );
 }
